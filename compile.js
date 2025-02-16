17b155ee3a766c4922e4ec7c1ad0eb8070225aca
eliyas41 <fentaeliyas@gmail.com>
2025-02-16 23:01:37 +0300

    block chain simple contracts

---
+++ compile.js
@@ -0,0 +1,8 @@
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+const source = fs.readFileSync(inboxPath, 'utf8');
+const output = solc.compile(source, 1).contracts[':Inbox'];
+console.log(output);
