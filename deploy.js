91946f25fd8ac834129b7a4a0342a5ae2d5e6656
eliyas41 <fentaeliyas@gmail.com>
2025-02-17 17:15:13 +0300

    02-027 Deployment to Testnet

--- deploy.js
+++ deploy.js
@@ -1,5 +1,5 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
-const Web3 = require('web3');
+const { Web3 } = require('web3');
 const { interface, bytecode } = require('./compile');
 const dotenv = require('dotenv');
 dotenv.config();
@@ -10,3 +10,16 @@ const provider = new HDWalletProvider(
 );
 
 const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+  // console.log(accounts);
+  const inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .send({ from: accounts[0], gas: '1000000' });
+
+  console.log(inbox.options.address);
+  provider.engine.stop();
+}
+
+deploy();
