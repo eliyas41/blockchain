9fa69320fd3acea70fe66b3c2d5e70d2b4d786e1
eliyas41 <fentaeliyas@gmail.com>
2025-02-17 14:36:32 +0300

    Refactore to Async Await

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -4,18 +4,19 @@ const { Web3 } = require('web3');
 
 const web3 = new Web3(ganache.provider());
 
-beforeEach(() => {
+let accounts;
+
+beforeEach(async () => {
   // get a list of all accounts
-  web3.eth.getAccounts()
-    .then(fetchAccounts => {
-      console.log(fetchAccounts);
-    });
+  accounts = await web3.eth.getAccounts()
   // use one of those accounts to deploy the contract
   // use one of those accounts to make calls to the contract
 })
 
 describe('Inbox', () => {
-  it('deploys a contract', () => { })
+  it('deploys a contract', () => {
+    console.log(accounts)
+  })
 })
 
 
