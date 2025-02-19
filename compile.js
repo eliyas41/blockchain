f539f62666003769a162c3ec8c645c1d1572da7a
eliyas41 <fentaeliyas@gmail.com>
2025-02-19 07:25:38 +0300

    update compile.js to latest one

--- compile.js
+++ compile.js
@@ -4,6 +4,23 @@ const solc = require('solc');
 
 const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
 const source = fs.readFileSync(inboxPath, 'utf8');
-const output = solc.compile(source, 1).contracts[':Inbox'];
 
-module.exports = output;
+const input = {
+  language: 'Solidity',
+  sources: {
+    'Inbox.sol': {
+      content: source,
+    },
+  },
+  settings: {
+    outputSelection: {
+      '*': {
+        '*': ['*'],
+      },
+    },
+  },
+};
+
+module.exports = JSON.parse(solc.compile(JSON.stringify(input))).contracts[
+  'Inbox.sol'
+].Inbox;
