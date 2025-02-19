c7a60b4c5b42a80ef4acf5d8e25aa178f24a8f2e
eliyas41 <fentaeliyas@gmail.com>
2025-02-19 22:45:17 +0300

    Entering the lottery

--- lottery smart_contract/lottery.sol
+++ lottery smart_contract/lottery.sol
@@ -3,5 +3,15 @@
 pragma solidity 0.8.19;
 
 contract Lottery{
-    address public manager;
+    address public  manager;
+    address[] public players;
+
+    constructor() {
+        manager = msg.sender;
+    }
+
+    function enter() public payable  {
+        // require(msg.sender != address(0), "Entrance is invalid");
+        players.push(msg.sender);
+    }
 }
