387ab8ab1b50dde529bcff13ec4bb408626e12c5
eliyas41 <fentaeliyas@gmail.com>
2025-02-20 00:29:39 +0300

    Sending Ether from contracts

--- lottery smart_contract/lottery.sol
+++ lottery smart_contract/lottery.sol
@@ -4,7 +4,8 @@ pragma solidity 0.8.19;
 
 contract Lottery{
     address public  manager;
-    address[] public players;
+//     address[] public players;  //the old one from the course 
+    address payable[] public players;
 
     constructor() {
         manager = msg.sender;
@@ -12,15 +13,23 @@ contract Lottery{
 
     function enter() public payable  {
         require(msg.value < .01 ether, "The amount should be greater than .01 ether");
-        players.push(msg.sender);
+        // players.push(msg.sender); //The old one which is depriciated 
+        players.push(payable(msg.sender));
     }
 
     function random() private view returns (uint) {
     return uint(keccak256(abi.encodePacked(block.prevrandao, block.timestamp, players)));
     }
 
-    function pickWinner() public view returns(address){
-        uint index = random();
-        return players[index % players.length];
+       function pickWinner() public {
+        // require(players.length > 0, "No players in the lottery");
+
+        uint index = random() % players.length;
+        address payable winner = players[index];
+
+        (bool success, ) = winner.call{value: address(this).balance}(""); // Transfer funds
+        require(success, "Transfer failed");
+
+        // players = new address payable ; // Reset players array for next round
     }
 }
