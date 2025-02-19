3d2bd33397e196cafdbfbf378cd1b30177ddf850
eliyas41 <fentaeliyas@gmail.com>
2025-02-19 07:27:22 +0300

    update deploy.js to latest one

--- deploy.js
+++ deploy.js
@@ -1,25 +1,26 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const { Web3 } = require('web3');
-const { interface, bytecode } = require('./compile');
-const dotenv = require('dotenv');
-dotenv.config();
 
-const provider = new HDWalletProvider(
-  process.env.ACCOUNT_MNENONIC,
-  process.env.INFURA_URL
+const { abi, evm } = require('./compile');
+
+provider = new HDWalletProvider(
+  'YOUR_MNEMONIC',
+  'YOUR_INFURA_URL'
 );
 
 const web3 = new Web3(provider);
 
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
-  // console.log(accounts);
-  const inbox = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ['Hi there!'] })
-    .send({ from: accounts[0], gas: '1000000' });
 
-  console.log(inbox.options.address);
-  // provider.engine.stop();
-}
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
 
 deploy();
