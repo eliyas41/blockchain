e721ab87d827d0060863ca43c4efdddcddcfbdae
eliyas41 <fentaeliyas@gmail.com>
2025-02-24 23:01:53 +0300

    update lottery cotract

--- lottery smart_contract/contracts/Lottery.sol
+++ lottery smart_contract/contracts/Lottery.sol
@@ -2,34 +2,42 @@
 
 pragma solidity 0.8.19;
 
-contract Lottery{
-    address public  manager;
-//     address[] public players;  //the old one from the course 
+contract Lottery {
+    address public manager;
+    //     address[] public players;  //the old one from the course
     address payable[] public players;
 
     constructor() {
         manager = msg.sender;
     }
 
-    function enter() public payable  {
-        require(msg.value < .01 ether, "The amount should be greater than .01 ether");
-        // players.push(msg.sender); //The old one which is depriciated 
+    function enter() public payable {
+        require(
+            msg.value > .01 ether,
+            "The amount should be greater than .01 ether"
+        );
+        // players.push(msg.sender); //The old one which is depriciated
         players.push(payable(msg.sender));
     }
 
     function random() private view returns (uint) {
-    return uint(keccak256(abi.encodePacked(block.prevrandao, block.timestamp, players)));
+        return
+            uint(
+                keccak256(
+                    abi.encodePacked(block.prevrandao, block.timestamp, players)
+                )
+            );
     }
 
-    function pickWinner() public restricted{
+    function pickWinner() public restricted {
         require(players.length > 0, "No players in the lottery");
 
         uint index = random() % players.length;
         address payable winner = players[index];
 
-        (bool success,) = winner.call{value: address(this).balance}("");
-        require(success,"Transfer failed");
-        
+        (bool success, ) = winner.call{value: address(this).balance}("");
+        require(success, "Transfer failed");
+
         // Fix: Create a new array with zero elements
         players = new address payable[](0);
     }
@@ -40,7 +48,7 @@ contract Lottery{
     }
 
     // function to get all players
-    function getPlayers() public view returns(address payable[] memory){
+    function getPlayers() public view returns (address payable[] memory) {
         return players;
     }
 }
